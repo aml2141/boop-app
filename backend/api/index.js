c261fbeeeb3892f51704583cd1cf8ecc35d2c6da
Adam Mark Lampell <adamlampell@Mac.lan>
2025-10-08 18:27:49 -0400

    Fix CORS for production domain

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,7 +4,10 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://helloboop.com', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Create Checkout Session
