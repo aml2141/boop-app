312117bc90013f492dcfad62a5384906db33b520
Adam Mark Lampell <adamlampell@Mac.lan>
2025-10-11 23:20:07 -0400

    Update CORS to allow www subdomain

--- backend/api/index.js
+++ backend/api/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: ['https://helloboop.com', 'http://localhost:5173'],
+  origin: ['https://helloboop.com', 'https://www.helloboop.com', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
